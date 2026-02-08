d59631330efbaa40ee7c5061f366946f8e414c38
Michal Sar <michal_sar@onet.pl>
2026-02-08 11:05:10 +0100

    minor fixes

--- sokoban_backend/settings.py
+++ sokoban_backend/settings.py
@@ -87,12 +87,7 @@ WSGI_APPLICATION = 'sokoban_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default='REMOVED',
-        conn_max_age=600
-    )
-}
+DATABASES = {'default': dj_database_url.config(conn_max_age=600)}
 
 
 # Password validation
